71fe789fa44cd1ab6c9644e6c5f7f09a683b2337
WGTECH\anh.le <anh.le@beteasy.com.au>
2021-08-07 18:16:48 +1000

    feat: improve type declaration

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,7 +4,7 @@ export const Counter = () => {
   const [count, setCount] = useState(0)
   return (
     <div>
-      <h3>Update the count and edit src/App.tsx, state is preserved ?</h3>
+      <h3>Update the count and edit src/App.tsx, state is preserved ??</h3>
       <button onClick={() => setCount((c) => c + 1)}>Count - {count}</button>
     </div>
   )
