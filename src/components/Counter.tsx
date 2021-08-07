951b15071ce05942873dd4f21277b2592442e891
WGTECH\anh.le <anh.le@beteasy.com.au>
2021-08-07 19:27:08 +1000

    feat: adding redux store

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
+import { useSelector, useDispatch } from "react-redux";
+import { increment } from "../redux/counterSlice";
+import { RootState } from "../redux/store";
 
 export const Counter = () => {
-  const [count, setCount] = useState(0)
+  const { count } = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <h3>Update the count and edit src/App.tsx, state is preserved ??</h3>
-      <button onClick={() => setCount((c) => c + 1)}>Count - {count}</button>
+      <h3>Update the count and edit src/App.tsx, state is preserved ?</h3>
+      <button 
+        onClick={() => {
+          dispatch(increment())
+        }}
+      >
+        {count}
+      </button>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,16 @@
 import ReactDOM from 'react-dom'
 import { App } from './App'
+import store from './redux/store'
+import { Provider } from 'react-redux'
+import { StrictMode } from 'react'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const rootElement = document.getElementById('root')
+
+ReactDOM.render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
+  rootElement
+)
