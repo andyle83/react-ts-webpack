87f9fafe02f13248e5fa5150a36618f827561364
WGTECH\anh.le <anh.le@beteasy.com.au>
2021-08-07 16:02:29 +1000

    feat: add lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import './styles.css';
-import react_logo from './react.png';
-import { Counter } from './components/Counter';
+import './styles.css'
+import react_logo from './react.png'
+import { Counter } from './components/Counter'
 
 export const App = () => {
   return ( 
